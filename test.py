3eb55738e2047bb3f58aed7c8b07ba098bbf9bf3
collinspg <collinsokolie90@gmail.com>
2023-11-09 15:25:06 +0100

    tested a new python code

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
-numbers = []
+# numbers = []
 
-while True:
-    num = float(input("Enter a number (enter 0 to stop): "))
-    if num == 0:
-        break
-    numbers.append(num)
+# while True:
+#     num = float(input("Enter a number (enter 0 to stop): "))
+#     if num == 0:
+#         break
+#     numbers.append(num)
 
-# Filter positive numbers
-positive_numbers = [num for num in numbers if num > 0]
+# # Filter positive numbers
+# positive_numbers = [num for num in numbers if num > 0]
 
-# Find the smallest positive number
-smallest_positive = min(positive_numbers, default=None)
+# # Find the smallest positive number
+# smallest_positive = min(positive_numbers, default=None)
 
-# Print the result
-if smallest_positive is None:
-    print("No positive numbers were entered.")
-else:
-    print("Smallest positive number:", smallest_positive)
+# # Print the result
+# if smallest_positive is None:
+#     print("No positive numbers were entered.")
+# else:
+#     print("Smallest positive number:", smallest_positive)
+def fullname(w1,w2):
+  return w1 + ' ' + w2
+
+f=fullname(w2='faith',w1='charity')
+print(f)
